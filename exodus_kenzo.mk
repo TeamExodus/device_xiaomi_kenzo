b5387deb879d61f7751b9275739acec53e815e65
Raja M <Raja1612.M@gmail.com>
2016-11-27 22:09:50 +0530

    kenzo: add proper telephony paths
    Change-Id: Ifc5e9b24533f6ee79e93c25dd9d7f96cf1fe4ab2

--- exodus_kenzo.mk
+++ exodus_kenzo.mk
@@ -16,13 +16,14 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common Exodus stuff.
 $(call inherit-product, vendor/exodus/config/common_full_phone.mk)
 
 # Inherit from kenzo device
 $(call inherit-product, device/xiaomi/kenzo/device.mk)
+
 
 # Set those variables here to overwrite the inherited values.
 BOARD_VENDOR := Xiaomi
